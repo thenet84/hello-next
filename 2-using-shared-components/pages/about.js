78fada0f72b84bc19b744059724733a4314a401f
Denet <the.net.wo@gmail.com>
2019-09-09 16:02:49 +0100

    Use shared components

---
+++ 2-using-shared-components/pages/about.js
@@ -0,0 +1,12 @@
+import Layout from '../components/Layout';
+
+const About = () => (
+    <div>
+        <Layout>
+            
+        </Layout>
+        <p>This is about page</p>
+    </div>
+);
+
+export default About;
---
+++ 2-using-shared-components/pages/index.js
@@ -0,0 +1,13 @@
+import Link  from 'next/link';
+
+import Layout from '../components/Layout';
+
+const Index = () => (
+    <div>
+        <Layout> 
+            <p>Hello Next.js</p>
+        </Layout>
+    </div>
+);
+
+export default Index;
